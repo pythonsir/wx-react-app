58ddabaf320fe1cab9a9380881a8a4554a4fe2b2
python <baidu211@vip.qq.com>
2018-03-05 13:11:25 +0800

    修改启动脚本

--- scripts/start.js
+++ scripts/start.js
@@ -1,6 +1,5 @@
 'use strict';
 
-console.log("eeeeeeeee")
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
